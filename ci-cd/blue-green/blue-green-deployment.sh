e0eeaaa8bc757921b5977c09f4cf1c9053df5a77
devchloe <devchloe.kwon@gmail.com>
2018-10-24 17:07:40 +0900

    add a few steps to show blue_version

--- ci-cd/blue-green/blue-green-deployment.sh
+++ ci-cd/blue-green/blue-green-deployment.sh
@@ -3,12 +3,18 @@
 # blue-green-deployment.sh <namespace> <servicename> <new-version> <new-deployment-file-path>
 # Deployment name should be <service>-<version> -- Green Deployment Pod 준비되어 있는지 확인
 
+#CONTEXT=$1
 NAMESPACE=$1
 SERVICE_NAME=$2
 NEW_VERSION=$3
 NEW_DEPLOYMENT_FILE_PATH=$4
 
 echo "Starting green deployement."
+
+#kubectl config use-context $KUBE_CONTEXT
+export OLD_VERSION=$(kubectl get svc ${SERVICE_NAME} -n ${NAMESPACE} -o jsonpath='{.spec.selector.version}')
+echo "Blue version is ${OLD_VERSION}."
+#deploy new version? yes or no
 
 kubectl apply -f $NEW_DEPLOYMENT_FILE_PATH
 
@@ -30,8 +36,9 @@ done
 
 echo "Deployment has minimum availability."
 
-kubectl patch svc $SERVICE_NAME -n $NAMESPACE -p "{\"spec\": {\"selector\": {\"name\": \"${SERVICE_NAME}\", \"version\": \"${NEW_VERSION}\"}}}"
 
+kubectl patch svc $SERVICE_NAME -n $NAMESPACE -p "{\"spec\": {\"selector\": {\"name\": \"${SERVICE_NAME}\", \"version\": \"${NEW_VERSION}\"}}}"
+#kubectl delete deployment $SERVICE_NAME-$OLD_VERSION -n $NAMESPACE
 echo "Request Testing: "
 echo $(curl -s http://$(minikube ip):$(kubectl get svc nginx -n ci-cd -o jsonpath="{.spec.ports[*].nodePort"})/version | grep nginx)
 echo "Done."
