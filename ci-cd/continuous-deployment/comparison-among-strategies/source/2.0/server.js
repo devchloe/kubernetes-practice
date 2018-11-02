100e82ee01d00d94db85f9e5e8f17fc5fc723ffc
devchloe <devchloe.kwon@gmail.com>
2018-11-02 20:39:20 +0900

    node.js web app for docker container image

---
+++ ci-cd/continuous-deployment/comparison-among-strategies/source/2.0/server.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+const PORT = 8080;
+const HOST = '0.0.0.0';
+
+app.get('/', (req, res, next) => {
+    res.send('Hello version 1.0\n');
+});
+
+app.get('/health', (req, res, next) => {
+    res.send('/health called..\n');
+})
+
+app.get('/ready', (req, res, next) => {
+    res.send('/ready called..\n');
+})
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
+
